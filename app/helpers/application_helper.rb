57a803ca8bcdd2e13176c92ca7ab6ac67b539342
Yash <yashpandey1106@gmail.com>
2022-02-21 15:56:39 +0530

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title="")
+        base_title="Ruby On Rails"
+        if page_title==""
+            return base_title
+        else
+            return page_title + " | " + base_title
+        end
+    end
 end
