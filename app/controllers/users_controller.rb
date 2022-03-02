420df5955db86bb90c215a54601dd4b037674be6
Yash <yashpandey1106@gmail.com>
2022-03-02 18:19:48 +0530

    Finish User crud operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -20,8 +28,47 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your data updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
+
+    end
+  end
+
+  def destroy
+    
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success]="User deleted successfully"
+    redirect_to users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in'
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user == @user
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_120803) do
+ActiveRecord::Schema.define(version: 2022_03_01_151131) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_120803) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
